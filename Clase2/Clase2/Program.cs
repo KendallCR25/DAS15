c1d2d8103ff741e2be94d4d7477d45872bcaa5d0
KendallCR25 <kendallcr25@gmail.com>
2015-05-18 20:23:15 -0600

    Cambio

--- Clase2/Clase2/Program.cs
+++ Clase2/Clase2/Program.cs
@@ -12,7 +12,9 @@ namespace Clase2
         {
             Console.WriteLine("Hello world");
             Console.WriteLine("Press any key to exit");
+            Console.
             Console.ReadKey();
+            // :v
         }
     }
 }
